f3108bcca547e406f92b0faf16d02f57a66da58e
guenerkusci <guener.kusci@gmail.com>
2026-08-09 19:41:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 
-const CACHE='familycircle-v8';
-const ASSETS=['./','index.html','styles.css?v=8','app.js?v=8','manifest.json','icon-192.png','icon-512.png'];
+const CACHE='familycircle-v8-1';
+const ASSETS=['./','index.html','styles.css?v=8.1','app.js?v=8.1','manifest.json','icon-192.png','icon-512.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
